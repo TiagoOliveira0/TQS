a5fd18c4803d470e961390efbfaa09192043c55b
Tiago Oliveira <tiago.srv.oliveira@ua.pt>
2021-03-24 15:56:11 +0000

    NullerPointerException no exercício 2 da Lab2

---
+++ Lab2/Ex2/gs-mockForHttpClient/src/test/java/geocoding/AddressResolverTest.java
@@ -0,0 +1,59 @@
+package geocoding;
+
+import connection.ISimpleHttpClient;
+import org.json.simple.parser.ParseException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
+
+import javax.management.BadAttributeValueExpException;
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+@MockitoSettings(strictness = Strictness.LENIENT)
+class AddressResolverTest {
+    @Mock ISimpleHttpClient httpclient;
+
+    @Test
+    void whenGoodAlboiGps_returnAddress() throws ParseException, IOException, URISyntaxException {
+        //todo
+        //Não sei que esta linha é preciso ou não, mas dá erro de ambas as maneiras
+        //ISimpleHttpClient htttpclient = Mockito.mock(ISimpleHttpClient.class);
+        String s = "{\"info\":{\"statuscode\":0,\"copyright\":{\"text\":\"\\u00A9 2021 MapQuest, Inc.\",\"imageUrl\":\"http://api.mqcdn.com/res/mqlogo.gif\",\"imageAltText\":\"\\u00A9 2021 MapQuest, Inc.\"},\"messages\":[]},\"options\":{\"maxResults\":1,\"thumbMaps\":true,\"ignoreLatLngInput\":false},\"results\":[{\"providedLocation\":{\"latLng\":{\"lat\":40.640661,\"lng\":-8.656688}},\"locations\":[{\"street\":\"Cais do Alboi\",\"adminArea6\":\"\",\"adminArea6Type\":\"Neighborhood\",\"adminArea5\":\"Gl\\u00F3ria e Vera Cruz\",\"adminArea5Type\":\"City\",\"adminArea4\":\"\",\"adminArea4Type\":\"County\",\"adminArea3\":\"Centro\",\"adminArea3Type\":\"State\",\"adminArea1\":\"PT\",\"adminArea1Type\":\"Country\",\"postalCode\":\"3800-246\",\"geocodeQualityCode\":\"B1AAA\",\"geocodeQuality\":\"STREET\",\"dragPoint\":false,\"sideOfStreet\":\"N\",\"linkId\":\"0\",\"unknownInput\":\"\",\"type\":\"s\",\"latLng\":{\"lat\":40.640524,\"lng\":-8.656713},\"displayLatLng\":{\"lat\":40.640524,\"lng\":-8.656713},\"mapUrl\":\"http://open.mapquestapi.com/staticmap/v5/map?key=uXSAVwYWbf9tJmsjEGHKKAo0gOjZfBLQ&type=map&size=225,160&locations=40.64052368145179,-8.656712986761146|marker-sm-50318A-1&scalebar=true&zoom=15&rand=1063335095\",\"roadMetadata\":null}]}]}";
+        Mockito.when(httpclient.get("http://open.mapquestapi.com/geocoding/v1/reverse?key=uXSAVwYWbf9tJmsjEGHKKAo0gOjZfBLQ&location=40.640661,%20-8.656688&includeRoadMetadata=true")).thenReturn(s);
+
+        //test
+        AddressResolver resolver = new AddressResolver(httpclient);
+        Address result = resolver.findAddressForLocation(40.640661, -8.656688);
+
+        //return
+        assertEquals( result, new Address( "Cais do Alboi", "Glória e Vera Cruz", "Centro", "3800-246", null) );
+
+    }
+
+    @Test
+    public void whenBadCoordidates_throwBadArrayindex() throws IOException, URISyntaxException, ParseException {
+
+        AddressResolver resolver = new AddressResolver(httpclient);
+        Assertions.assertThrows(BadAttributeValueExpException.class, () ->{
+            Address result = resolver.findAddressForLocation(91,-0);
+        });
+
+        Assertions.assertThrows(BadAttributeValueExpException.class, () ->{
+            Address result = resolver.findAddressForLocation(91,-181);
+        });
+
+        Assertions.assertThrows(BadAttributeValueExpException.class, () ->{
+            Address result = resolver.findAddressForLocation(89,-181);
+        });
+
+    }
+}
