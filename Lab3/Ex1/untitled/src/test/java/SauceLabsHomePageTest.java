a29ddbc42f6bca5bb21a7dd103738b0afbb59a9c
Tiago Oliveira <tiago.srv.oliveira@ua.pt>
2021-04-06 16:31:49 +0100

    Lab3 ex1, está a dar erro acho que é por causa da versão do junit, Lab3 ex2, esta a dar erro por timeout acho eu, os teste correu bem a correr no selenium mas no intellij está a dar erro

---
+++ Lab3/Ex1/untitled/src/test/java/SauceLabsHomePageTest.java
@@ -0,0 +1,34 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+
+class SauceLabsHomePageTest {
+    private WebDriver browser;
+
+
+    @BeforeEach
+    void setup(){
+        System.setProperty("webdriver.gecko.driver", "/opt/webdriver/bin/geckodriver");
+        WebDriver browser = new FirefoxDriver();
+    }
+
+    @AfterEach
+    void teardown(){
+        browser.close();
+    }
+
+    @Test
+    public void site_header_is_on_home_page() {
+        browser.get("https://www.saucelabs.com");
+        WebElement href = browser.findElement(By.xpath("//a[@href='https://accounts.saucelabs.com/']"));
+        assertTrue((href.isDisplayed()));
+    }
+}
