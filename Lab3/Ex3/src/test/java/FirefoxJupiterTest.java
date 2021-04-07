c061ecbc0bfba7f361c26a5bb8a05dfd6adb4e9d
Tiago Oliveira <tiago.srv.oliveira@ua.pt>
2021-04-07 21:00:39 +0100

    Exercicio 3 finalizado, continua a ter timeout error after x ms nos ex1 e ex2

---
+++ Lab3/Ex3/src/test/java/FirefoxJupiterTest.java
@@ -0,0 +1,45 @@
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import io.github.bonigarcia.seljup.SeleniumJupiter;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+@ExtendWith(SeleniumJupiter.class)
+class FirefoxJupiterTest {
+    FirefoxDriver driver;
+
+    public void FireFoxJupiterTest(FirefoxDriver driver){
+        this.driver=driver;
+    }
+
+    @Test
+    void testWithFirefox(FirefoxDriver firefoxDriver) {
+        // Use Firefox in this test
+        FireFoxJupiterTest(new FirefoxDriver());
+        driver.get("https://www.ua.pt");
+        assertThat(driver.getTitle(), containsString("Universidade de Aveiro"));
+    }
+
+    @Test
+    void testWithChromeAndFirefox(FirefoxDriver driver1, ChromeDriver driver2) {
+        // Use Chrome and Firefox in this test
+        driver1.get("http://www.seleniumhq.org/");
+        driver2.get("http://www.junit.org/junit5/");
+        assertThat(driver1.getTitle(), startsWith("Selenium"));
+        assertThat(driver2.getTitle(), equalTo("JUnit 5"));
+    }
+
+    @Test
+    void testWithHeadlessBrowser(HtmlUnitDriver driver){
+        System.setProperty("webdriver.gecko.driver", "/opt/webdriver/bin/geckodriver");
+        FireFoxJupiterTest(new FirefoxDriver());
+        driver.get("https://www.ua.pt");
+        assertThat(driver.getTitle(), containsString("Universidade de Aveiro"));
+    }
+
+}
