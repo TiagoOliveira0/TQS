6f74bf62af20d0a76918aca75273a9a11e6ccabe
Tiago Oliveira <tiago.srv.oliveira@ua.pt>
2021-05-05 17:03:02 +0100

    Lab 7 finalizada, contudo ex2 a dar erro de imports

---
+++ Lab7/Ex3/src/main/java/CarRepository.java
@@ -0,0 +1,10 @@
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import javax.transaction.Transactional;
+
+@Repository
+@Transactional
+public interface CarRepository  extends JpaRepository<Car, Long> {
+    public Car findByName(String name);
+}
